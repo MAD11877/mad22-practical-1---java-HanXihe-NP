9a711fa24fe6746db78a9931932496cb726f1bb0
HanXihe-NP <103987209+HanXihe-NP@users.noreply.github.com>
2022-04-19 19:45:50 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -11,6 +11,7 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    
+    int userInteger = in.nextInt();
+    System.out.println(userInteger * userInteger);
   }
 }
