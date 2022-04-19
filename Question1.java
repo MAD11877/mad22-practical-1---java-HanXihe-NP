e45e720b6237853dced36052f3b04972004010ee
HanXihe-NP <103987209+HanXihe-NP@users.noreply.github.com>
2022-04-19 20:52:07 +0800

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -7,6 +7,6 @@ public class Question1
      * System.out.println("Hello World!");
      * Hint: System.out.println(...)
      */
-    System.out.println("Hello World!");
+    System.out.print("Hello World!");
   }
 }
